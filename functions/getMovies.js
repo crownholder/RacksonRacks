339dbf570b5aba0c91dc782c0f4e38bf23922ac6
Michael Kees <80150652+crownholder@users.noreply.github.com>
2021-11-06 00:35:59 -0700

    Update getMovies.js

--- functions/getMovies.js
+++ functions/getMovies.js
@@ -2,21 +2,21 @@ const fetch = require('node-fetch')
 
 exports.handler = async function (event) {
   const body = JSON.parse(event.body)
-  const genre = body.genre
+  const reflections = body.reflections
   const pageState = body.pageState
   const url = process.env.ASTRA_GRAPHQL_ENDPOINT
   const query = `
   query {
-    movies_by_genre (
+    coins_by_reflection (
       value: { genre: ${JSON.stringify(genre)}},
       orderBy: [year_DESC],
       options: { pageSize: 6, pageState: ${JSON.stringify(pageState)} }
     ) {
       values {
         year,
-        title,
-        duration,
-        synopsis,
+        name,
+        total,
+        about,
         thumbnail
       }
       pageState
